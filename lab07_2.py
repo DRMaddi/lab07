b9bc99233c090f8dffd47b78b1963388fab42ef0
DRMaddi <44313650+DRMaddi@users.noreply.github.com>
2023-11-11 22:25:06 +0530

    Add files via upload

--- lab07_2.py
+++ lab07_2.py
@@ -5,7 +5,7 @@ from sklearn import svm
 # Read data from the Excel file
 df = pd.read_excel(r"C:\Users\anish\Downloads\embeddingsdata.xlsx")
 
-# Extract data from the columns 'embed_0' and 'embed_1'
+# Extract data from the columns 'embed_0' and 'embed_1' for training
 X_train = df[['embed_0', 'embed_1']]  # Features
 y_train = df['Label']  
 
@@ -21,3 +21,15 @@ support_vectors = clf.support_vectors_
 # Now you can study the support vectors
 print("Support Vectors:")
 print(support_vectors)
+
+
+accuracy = clf.score(X_train, y_train)
+
+# Print the accuracy
+print(f"Accuracy: {accuracy:.2f}")
+
+
+predicted_class = clf.predict(support_vectors)
+
+# Print the predicted class
+print(f"Predicted class for test_vect: {predicted_class[0]}")
